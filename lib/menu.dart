e9df957cb6585ddfecdeb334fd256d04418b3979
Joshua ESS <158529708+greencrabby@users.noreply.github.com>
2024-11-06 12:38:19 +0700

    finished tutorial 7

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/widgets/left_drawer.dart';
+import 'package:mental_health_tracker/widgets/mood_card.dart';
 
 class MyHomePage extends StatelessWidget {
   final String npm = '2306275001'; // NPM
@@ -29,7 +31,11 @@ class MyHomePage extends StatelessWidget {
         ),
         // The background color of the AppBar is obtained from the application theme color scheme.
         backgroundColor: Theme.of(context).colorScheme.primary,
+        // Set drawer icon color to white
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
+      // Add drawer as a parameter value for the drawer attribute of the Scaffold widget
+      drawer: const LeftDrawer(),
       // Body of the page with paddings around it.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -93,13 +99,6 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class ItemHomepage {
-    final String name;
-    final IconData icon;
-
-    ItemHomepage(this.name, this.icon);
-}
-
 class InfoCard extends StatelessWidget {
   // Card information that displays the title and content.
 
@@ -127,59 +126,6 @@ class InfoCard extends StatelessWidget {
             const SizedBox(height: 8.0),
             Text(content),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class ItemCard extends StatelessWidget {
-  // Display the card with an icon and name.
-
-  final ItemHomepage item; 
-  
-  const ItemCard(this.item, {super.key}); 
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      // Specify the background color of the application theme.
-      color: Theme.of(context).colorScheme.secondary,
-      // Round the card border.
-      borderRadius: BorderRadius.circular(12),
-      
-      child: InkWell(
-        // Action when the card is pressed.
-        onTap: () {
-          // Display the SnackBar message when the card is pressed.
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("You have pressed the ${item.name} button!"))
-            );
-        },
-        // Container to store the Icon and Text
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              // Place the Icon and Text in the center of the card.
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
